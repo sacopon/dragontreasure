4f9f43228f587d6bff037124e7e9f21fbd89e2a4
sacopon <sacopon.dev@gmail.com>
2023-02-19 12:40:48 +0900

    :sparkles: adjust canvas size

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,75 @@
 import init, { greet, greet_to_pixi } from "../wasm/pkg";
 import { Application, Text, Assets, Sprite, Texture, BaseTexture } from "pixi.js";
+import { disableOuterCanvasTouchEvent } from "./presentation/disable_touch_event";
+import { Screen } from "./presentation/screen";
 
 // Rust 側から呼び出すことが可能なメソッドを設定する
 const bindFunction = (name: string, func: any) => {
   (window as any)[name] = func;
 }
 
-async function mainProgram() {
-  // TypeScript からコンソールへの出力
-  console.log("Hello from TypeScript");
-  // コンソールへの出力メソッドをWebAssembly(Rust)から呼び出せるようにする
-  bindFunction("jsConsoleLog", (msg: string) => console.log(msg));
+class Main {
+  private screen: Screen | null = null;
 
-  // wasm 初期化
-  await init();
-  // wasm 呼び出し(コンソールへの出力)
-  greet("WebAssembly");
+  public async run() {
 
-  // pixi.js 初期化
-  const app = new Application({
-    backgroundColor: 0xFFFFFF,
-  });
-  document.body.appendChild(app.view as unknown as Node);
-  // フォントの読み込み
-  const font = new FontFace(
-    "PixelMplus12",
-    "url(/PixelMplus12-Bold.ttf)",
-    { style: "normal", weight: "bold" }
-  );
-  await font.load();
-  document.fonts.add(font);
+    // TypeScript からコンソールへの出力
+    console.log("Hello from TypeScript");
+    // コンソールへの出力メソッドをWebAssembly(Rust)から呼び出せるようにする
+    bindFunction("jsConsoleLog", (msg: string) => console.log(msg));
+  
+    // wasm 初期化
+    await init();
+    // wasm 呼び出し(コンソールへの出力)
+    greet("WebAssembly");
+  
+    // pixi.js 初期化
+    const app = new Application({
+      backgroundColor: 0xFFFFFF,
+    });
+    document.body.appendChild(app.view as unknown as Node);
+    disableOuterCanvasTouchEvent();
 
-  // 画像のロード/描画
-  Assets.addBundle("resources", { cat: "/cat.jpg" });
-  await Assets.loadBundle("resources");
-  const background = new Sprite(new Texture(Assets.get("cat") as BaseTexture));
-  background.anchor.set(0.5);
-  background.position.set(Math.floor(app.renderer.width / 2), Math.floor(app.renderer.height / 2));
-  app.stage.addChild(background);
+    // フォントの読み込み
+    const font = new FontFace(
+      "PixelMplus12",
+      "url(/PixelMplus12-Bold.ttf)",
+      { style: "normal", weight: "bold" }
+    );
+    await font.load();
+    document.fonts.add(font);
+  
+    // 画像のロード/描画
+    Assets.addBundle("resources", {
+      cat: "/cat.jpg",
+      test: "/test.json",
+    });
+    await Assets.loadBundle("resources");
+    const background = new Sprite(new Texture(Assets.get("cat") as BaseTexture));
+    app.stage.addChild(background);
 
-  // pixi.js のキャンバスへの文字列描画
-  const text1 = new Text("Hello pixi.js!!", { fontFamily: "PixelMplus12", fontWeight: "bold" });
-  app.stage.addChild(text1);
+    // pixi.js のキャンバスへの文字列描画
+    const text1 = new Text("Hello pixi.js!!", { fontFamily: "PixelMplus12", fontWeight: "bold" });
+    app.stage.addChild(text1);
+  
+    const text2 = new Text("", { fontFamily: "PixelMplus12", fontWeight: "bold" });
+    text2.y = 30;
+    app.stage.addChild(text2);
+    // pixi.js のキャンバスへの文字列描画メソッドをWebAssembly(Rust)から呼び出せるようにする
+    bindFunction("jsPrintPixiText", (msg: string) => { text2.text = msg; });
+    // wasm 呼び出し(pixi.js のキャンバスへの文字列描画)
+    greet_to_pixi("pixi.js");
 
-  const text2 = new Text("", { fontFamily: "PixelMplus12", fontWeight: "bold" });
-  text2.y = 30;
-  app.stage.addChild(text2);
-  // pixi.js のキャンバスへの文字列描画メソッドをWebAssembly(Rust)から呼び出せるようにする
-  bindFunction("jsPrintPixiText", (msg: string) => { text2.text = msg; });
-  // wasm 呼び出し(pixi.js のキャンバスへの文字列描画)
-  greet_to_pixi("pixi.js");
+    this.screen = new Screen(app);
+    window.addEventListener("resize", () => this.screen!.onResize());
+    // 最初の描画
+    window.dispatchEvent(new Event("resize"));
+  }
+
+  public static async run() {
+    const instance = new Main();
+    await instance.run();
+  }
 }
 
-window.addEventListener("DOMContentLoaded", mainProgram);
+window.addEventListener("DOMContentLoaded", Main.run);
